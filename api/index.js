5dc0ffcf058639ec66cc0f5afdbd2c971b70d7b8
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2023-12-06 22:25:49 +1030

    create and sign up api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
-dotenv.config();
+dotenv.config();//create dotenv then config to use
 
 mongoose.connect(process.env.MONGO).then(()=> {
     console.log("connected to MongoDB");
@@ -13,9 +14,10 @@ mongoose.connect(process.env.MONGO).then(()=> {
 
 const app= express();
 
-
+app.use(express.json());///
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
